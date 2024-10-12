02ce25152c993a351bd594c094a7f54b0c41ac34
zxfae <lecro.philippe@icloud.com>
2024-10-12 16:32:12 +0200

    Upd ip private

--- src/handlers.rs
+++ src/handlers.rs
@@ -1,4 +1,4 @@
-use crate::ip_info::fetch_ip_info;
+use crate::ip_info::{fetch_ip_info, IpInfoResult};
 use colored::*;
 
 pub fn handle_help() {
@@ -11,34 +11,43 @@ pub fn handle_full_name(full_name: &str) {
 
 pub async fn handle_ip(ip: &str) {
     println!("Starting handle_ip with IP: {}\n", ip);
-    if let Some(ip_info) = fetch_ip_info(ip).await {
-        if let Some(query) = ip_info.query {
-            println!("IP Address: {}", query);
-        }
-        if let Some(country) = ip_info.country {
-            println!("Country: {}", country);
-        }
-        if let Some(region) = ip_info.regionname {
-            println!("Region: {}", region);
-        }
-        if let Some(city) = ip_info.city {
-            println!("City: {}", city);
-        }
-        if let Some(zip) = ip_info.zip {
-            println!("Postal Code: {}", zip);
-        }
-        if let (Some(lat), Some(lon)) = (ip_info.lat, ip_info.lon) {
-            println!("Location (lat, long): {}, {}", lat, lon);
-        }
-        if let Some(isp) = ip_info.isp {
-            println!("ISP: {}", isp);
-        }
-        if let Some(org) = ip_info.org {
-            println!("Organization: {}", org);
-        }
-        if let Some(asn) = ip_info.asn {
-            println!("ASN: {}", asn);
-        }
+    match fetch_ip_info(ip).await {
+        Ok(IpInfoResult::Public(ip_info)) => {
+            if let Some(query) = ip_info.query {
+                println!("IP Address: {}", query);
+            }
+            if let Some(country) = ip_info.country {
+                println!("Country: {}", country);
+            }
+            if let Some(region) = ip_info.regionname {
+                println!("Region: {}", region);
+            }
+            if let Some(city) = ip_info.city {
+                println!("City: {}", city);
+            }
+            if let Some(zip) = ip_info.zip {
+                println!("Postal Code: {}", zip);
+            }
+            if let (Some(lat), Some(lon)) = (ip_info.lat, ip_info.lon) {
+                println!("Location (lat, long): {}, {}", lat, lon);
+            }
+            if let Some(isp) = ip_info.isp {
+                println!("ISP: {}", isp);
+            }
+            if let Some(org) = ip_info.org {
+                println!("Organization: {}", org);
+            }
+            if let Some(asn) = ip_info.asn {
+                println!("ASN: {}", asn);
+            }
+        },
+        Ok(IpInfoResult::Private(private_info)) => {
+            println!("IP Address: {}", private_info.ip);
+            println!("Class: {}", private_info.class);
+            println!("Range: {}", private_info.range);
+            println!("Purpose: {}", private_info.purpose);
+        },
+        Err(e) => println!("Error: {}", e),
     }
 }
 
--- src/ip_info.rs
+++ src/ip_info.rs
@@ -1,8 +1,8 @@
 use serde::Deserialize;
-use colored::*;
-use surf;
+use std::net::Ipv4Addr;
+use thiserror::Error;
 
-#[derive(Deserialize)]
+#[derive(Deserialize, Debug)]
 pub struct IpInfo {
     pub query: Option<String>,
     pub country: Option<String>,
@@ -16,26 +16,81 @@ pub struct IpInfo {
     pub asn: Option<String>,
 }
 
-pub async fn fetch_ip_info(ip: &str) -> Option<IpInfo> {
-    let url = format!("http://ip-api.com/json/{}", ip);
-    match surf::get(&url).await {
-        Ok(mut response) => {
-            if response.status().is_success() {
-                match response.body_json::<IpInfo>().await {
-                    Ok(data) => Some(data),
-                    Err(_) => {
-                        println!("{}", "Error: Failed to parse JSON response".red());
-                        None
-                    }
-                }
-            } else {
-                println!("{}", "Error: received non-200 response".red());
-                None
-            }
+#[derive(Debug)]
+pub struct PrivateIpInfo {
+    pub ip: String,
+    pub class: String,
+    pub range: String,
+    pub purpose: String,
+}
+
+pub enum IpInfoResult {
+    Public(IpInfo),
+    Private(PrivateIpInfo),
+}
+
+#[derive(Error, Debug)]
+pub enum IpInfoError {
+    #[error("HTTP error: {0}")]
+    HttpError(String),
+    #[error("Received non-200 response: {0}")]
+    NonSuccessResponse(u16),
+    #[error("Failed to parse JSON response: {0}")]
+    ParseError(#[from] serde_json::Error),
+    #[error("Invalid IP address")]
+    InvalidIpAddress,
+}
+
+impl From<surf::Error> for IpInfoError {
+    fn from(error: surf::Error) -> Self {
+        IpInfoError::HttpError(error.to_string())
+    }
+}
+
+fn get_private_ip_info(ip: &str) -> Result<PrivateIpInfo, IpInfoError> {
+    let ipv4: Ipv4Addr = ip.parse().map_err(|_| IpInfoError::InvalidIpAddress)?;
+
+    let (class, range, purpose) = if ipv4.is_private() {
+        if ipv4.octets()[0] == 10 {
+            ("A", "10.0.0.0 to 10.255.255.255", "Private network")
+        } else if ipv4.octets()[0] == 172 && (16..=31).contains(&ipv4.octets()[1]) {
+            ("B", "172.16.0.0 to 172.31.255.255", "Private network")
+        } else {
+            ("C", "192.168.0.0 to 192.168.255.255", "Private network")
         }
-        Err(_) => {
-            println!("{}", "Error: Failed to fetch IP info".red());
-            None
+    } else if ipv4.is_loopback() {
+        ("A", "127.0.0.0 to 127.255.255.255", "Loopback")
+    } else if ipv4.is_link_local() {
+        ("B", "169.254.0.0 to 169.254.255.255", "Link-local")
+    } else {
+        ("Unknown", "Unknown", "Special purpose")
+    };
+
+    Ok(PrivateIpInfo {
+        ip: ip.to_string(),
+        class: class.to_string(),
+        range: range.to_string(),
+        purpose: purpose.to_string(),
+    })
+}
+pub async fn fetch_ip_info(ip: &str) -> Result<IpInfoResult, IpInfoError> {
+    if is_private_or_reserved_ip(ip) {
+        get_private_ip_info(ip).map(IpInfoResult::Private)
+    } else {
+        let url = format!("http://ip-api.com/json/{}", ip);
+        let mut response = surf::get(&url).await.map_err(IpInfoError::from)?;
+
+        if response.status().is_success() {
+            let info = response.body_json::<IpInfo>().await.map_err(IpInfoError::from)?;
+            Ok(IpInfoResult::Public(info))
+        } else {
+            Err(IpInfoError::NonSuccessResponse(response.status().into()))
         }
     }
+}
+
+fn is_private_or_reserved_ip(ip: &str) -> bool {
+    ip.parse::<Ipv4Addr>()
+        .map(|ipv4| ipv4.is_private() || ipv4.is_loopback() || ipv4.is_link_local() || ipv4.is_broadcast())
+        .unwrap_or(false)
 }
